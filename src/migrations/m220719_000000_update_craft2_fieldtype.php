a4ec244c4f2bdb0d9bded6c136434447c2e8964a
Josh Crawford <joshua@sgroup.com.au>
2022-07-19 21:13:34 +1000

    Add Craft 2 migration

---
+++ src/migrations/m220719_000000_update_craft2_fieldtype.php
@@ -0,0 +1,26 @@
+<?php
+namespace verbb\usergroupfield\migrations;
+
+use verbb\usergroupfield\fields\UserGroupField;
+
+use Craft;
+use craft\db\Migration;
+
+class m220719_000000_update_craft2_fieldtype extends Migration
+{
+    // Public Methods
+    // =========================================================================
+
+    public function safeUp(): bool
+    {
+        $this->update('{{%fields}}', ['type' => UserGroupField::class], ['type' => 'UsergroupFieldFieldType']);
+
+        return true;
+    }
+
+    public function safeDown(): bool
+    {
+        echo "m220719_000000_update_craft2_fieldtype cannot be reverted.\n";
+        return false;
+    }
+}

d3212e790e2e773dcae4be1f19f83885d9b89d0a
Josh Crawford <joshua@sgroup.com.au>
2024-01-19 10:10:34 +1100

    Formatting

--- src/models/UserGroupCollection.php
+++ src/models/UserGroupCollection.php
@@ -5,18 +5,23 @@ use Craft;
 use craft\base\Model;
 use craft\elements\User;
 use craft\models\UserGroup;
+
 use yii\caching\TagDependency;
 
 class UserGroupCollection extends Model
 {
+    // Constants
+    // =========================================================================
+
+    public const CACHE_KEY = 'userGroupsCache';
+
+
     // Properties
     // =========================================================================
 
     public array $groupIds = [];
 
     private array $_groups = [];
-
-    const CACHE_KEY = 'userGroupsCache';
 
 
     // Public Methods
@@ -27,20 +32,18 @@ class UserGroupCollection extends Model
         return array_values($this->groupIds);
     }
 
-    public function getGroups($cache=false): array
+    public function getGroups(bool $cache = false): array
     {
         if (!$this->_groups) {
-
             $this->_groups = array_filter($this->_getAllGroups($cache), function(UserGroup $userGroup) {
                 return in_array($userGroup->uid, $this->getGroupIds(), false);
             });
-
         }
 
         return $this->_groups;
     }
 
-    public function inGroup(User $user = null, $cache = false): bool
+    public function inGroup(User $user = null, bool $cache = false): bool
     {
         if (!$user) {
             return false;
@@ -58,7 +61,7 @@ class UserGroupCollection extends Model
         return $inGroup;
     }
 
-    public function canAccess(User $user = null, $cache = false): bool
+    public function canAccess(User $user = null, bool $cache = false): bool
     {
         if (!$user) {
             return false;
@@ -67,28 +70,29 @@ class UserGroupCollection extends Model
         return $user->admin || $this->inGroup($user, $cache);
     }
 
+
+    // Private Methods
+    // =========================================================================
+
     private function _getUserGroups(): array
     {
         return Craft::$app->getUserGroups()->getAllGroups();
     }
 
-    private function _getAllGroups($cache = false): array
+    private function _getAllGroups(bool $cache = false): array
     {
-
-        if($cache)
-        {
+        if ($cache) {
+            $cacheService = Craft::$app->getCache();
             $cacheKey = self::CACHE_KEY;
-            $allGroups = Craft::$app->cache->get($cacheKey);
+            $allGroups = $cacheService->get($cacheKey);
 
             if ($allGroups === false) {
                 $allGroups = $this->_getUserGroups();
 
                 // Store the result in cache based on the url - will be cleard on entry save
-                Craft::$app->cache->set($cacheKey, $allGroups, 0, new TagDependency(['tags' => ['url:' . Craft::$app->getRequest()->getUrl()]]));
-
+                $cacheService->set($cacheKey, $allGroups, 0, new TagDependency(['tags' => ['url:' . Craft::$app->getRequest()->getUrl()]]));
             }
-        }
-        else {
+        } else {
             $allGroups = $this->_getUserGroups();
         }
 

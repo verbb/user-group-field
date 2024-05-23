2c7d2dc9faaa37b75ced8679365170f02367a4fa
Josh Crawford <joshua@sgroup.com.au>
2024-05-23 13:48:15 +1000

    Update English translations.

--- src/translations/en/user-group-field.php
+++ src/translations/en/user-group-field.php
@@ -3,6 +3,8 @@
 return [
   'Checkboxes' => 'Checkboxes',
   'Dropdown' => 'Dropdown',
+  'Mode' => 'Mode',
   'Radio buttons' => 'Radio buttons',
+  'Select the display mode' => 'Select the display mode',
   'User Group' => 'User Group',
 ];
